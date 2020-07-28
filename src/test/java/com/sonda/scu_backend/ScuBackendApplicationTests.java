77b881bfe410b2ee3d5e66e24d8d7bc1b0866869
ArnaldoSena <arnaldoufcg@gmail.com>
2020-07-28 20:41:08 -0300

    My first commit

---
+++ src/test/java/com/sonda/scu_backend/ScuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sonda.scu_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScuBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
